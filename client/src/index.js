715a9539e4bff0947871aff5656d0efffe186c17
michaelperryjnr <michaelperryt97@gmail.com>
2024-02-24 10:43:13 +0000

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,15 @@
-/**using parcel */
+ import React, { StrictMode } from "react";
+ import ReactDOM from "react-dom";
+ import App from "./App";
+
+ const root = document.getElementById("root");
+
+ const Render = () => {
+    return(
+        <StrictMode>
+            <App />
+        </StrictMode>
+    )
+ };
+
+ ReactDOM.render(React.createElement(Render), root);
